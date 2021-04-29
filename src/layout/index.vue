cc820aa71965c75751d4711cdb6b56d44e0b5996
shiguanghai <1932275668@qq.com>
2021-04-29 22:00:29 +0800

    使用 Element 处理布局和侧边栏导航

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="layout">
-    <h2>头部</h2>
-    <h2>侧边栏</h2>
-
-    <!-- 子路由出口 -->
-    <router-view />
-  </div>
+  <el-container>
+    <el-aside width="200px">
+      <app-aside />
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <!-- 子路由出口 -->
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import AppAside from './components/app-aside.vue'
+
 export default Vue.extend({
-  name: 'LayoutIndex'
+  name: 'LayoutIndex',
+  components: {
+    AppAside
+  }
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .el-header {
+    background-color: #B3C0D1;
+  }
+  .el-aside {
+    background-color: #D3DCE6;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+  }
+  .el-container {
+    min-height: 100vh;
+    min-width: 980px;
+  }
+</style>
