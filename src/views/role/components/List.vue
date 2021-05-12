7f1834c43146dba426845b5a07ca075e1857b6f9
shiguanghai <1932275668@qq.com>
2021-05-12 21:22:36 +0800

    角色管理 - 展示列表及列表的搜索、重置以及删除功能实现

---
+++ src/views/role/components/List.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="role-list">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- <span>筛选搜索</span> -->
+        <el-form ref="form" :model="form">
+          <el-form-item label="角色名称" prop="name">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              @click="onSubmit"
+              :disabled="loading"
+            >查询搜索</el-button>
+            <el-button
+              :disabled="loading"
+              @click="onReset"
+            >重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <el-button @click="dialogFormVisible = true">添加角色</el-button>
+      <el-table
+        :data="roles"
+        style="width: 100%"
+        v-loading="loading"
+      >
+        <el-table-column
+          prop="id"
+          label="编号"
+        />
+        <el-table-column
+          prop="name"
+          label="角色名称"
+        />
+        <el-table-column
+          prop="description"
+          label="描述"
+        />
+        <el-table-column
+          prop="createdTime"
+          label="添加时间"
+        />
+        <el-table-column
+          label="操作"
+          align="center"
+          width="150px"
+        >
+          <template slot-scope="scope">
+            <div>
+              <el-button type="text">分配菜单</el-button>
+              <el-button type="text">分配资源</el-button>
+            </div>
+            <div>
+              <el-button
+                type="text"
+                @click="handleEdit(scope.row)"
+              >编辑</el-button>
+              <el-button
+                size="mini"
+                type="text"
+                @click="handleDelete(scope.row)"
+              >删除</el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { getRoles, deleteRole } from '@/services/role'
+import { Form } from 'element-ui'
+
+export default Vue.extend({
+  name: 'RoleList',
+  data () {
+    return {
+      roles: [], // 角色列表
+      form: {
+        current: 1,
+        size: 50,
+        name: ''
+      }, // 查询条件
+      loading: false
+    }
+  },
+
+  created () {
+    this.loadRoles()
+  },
+
+  methods: {
+    async loadRoles () {
+      this.loading = true
+      const { data } = await getRoles(this.form)
+      this.roles = data.data.records
+      this.loading = false
+    },
+    onSubmit () {
+      this.loadRoles()
+    },
+    handleEdit (role: any) {
+      console.log(role)
+    },
+    async handleDelete (role: any) {
+      try {
+        await this.$confirm(`确认删除角色：${role.name}？`, '删除提示')
+        await deleteRole(role.id)
+        this.$message.success('删除成功')
+        this.loadRoles()
+      } catch (err) {
+        if (err && err.response) {
+          this.$message.error('删除失败，请重试')
+        } else {
+          this.$message.info('取消删除')
+        }
+      }
+    },
+    onReset () {
+      (this.$refs.form as Form).resetFields()
+      this.loadRoles()
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
