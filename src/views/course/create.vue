deb586d55add0af847bb6d57e99de7553e9a55f0
shiguanghai <1932275668@qq.com>
2021-05-20 20:50:03 +0800

    添加课程 表单结构完成 基本信息页数据绑定

--- src/views/course/create.vue
+++ src/views/course/create.vue
@@ -14,22 +14,35 @@
       <el-form label-width="80px" label-position="left">
         <div v-show="activeStep === 0">
           <el-form-item label="课程名称">
-            <el-input></el-input>
+            <el-input v-model="course.courseName"></el-input>
           </el-form-item>
           <el-form-item label="课程简介">
-            <el-input></el-input>
+            <el-input v-model="course.brief"></el-input>
           </el-form-item>
           <el-form-item label="课程概述">
-            <el-input type="textarea" ></el-input>
+            <el-input
+              style="margin-bottom: 10px"
+              v-model="course.previewFirstField"
+              type="textarea"
+              placeholder="概述1"
+            ></el-input>
+            <el-input
+              v-model="course.previewSecondField"
+              type="textarea"
+              placeholder="概述2"
+            ></el-input>
           </el-form-item>
           <el-form-item label="讲师姓名">
-            <el-input></el-input>
+            <el-input v-model="course.teacherDTO.teacherName"></el-input>
           </el-form-item>
           <el-form-item label="讲师简介">
-            <el-input></el-input>
+            <el-input v-model="course.teacherDTO.description"></el-input>
           </el-form-item>
           <el-form-item label="课程排序">
-            <el-input-number label="描述文字"></el-input-number>
+            <el-input-number
+              label="描述文字"
+              v-model="course.sortNum"
+            ></el-input-number>
           </el-form-item>
         </div>
         <div v-show="activeStep === 1">
@@ -44,7 +57,7 @@
               <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
           </el-form-item>
-          <el-form-item label="解锁封面">
+          <el-form-item label="介绍封面">
             <el-upload
               class="avatar-uploader"
               action="https://jsonplaceholder.typicode.com/posts/"
@@ -57,13 +70,65 @@
           </el-form-item>
         </div>
         <div v-show="activeStep === 2">
-          销售信息
+          <el-form-item label="售卖价格">
+            <el-input type="number">
+              <template slot="append">元</template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="商品原价">
+            <el-input type="number">
+              <template slot="append">元</template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="销量">
+            <el-input type="number">
+              <template slot="append">单</template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="活动标签">
+            <el-input></el-input>
+          </el-form-item>
         </div>
         <div v-show="activeStep === 3">
-          秒杀活动
+          <el-form-item label="限时秒杀">
+            <el-switch
+              v-model="isSeckill"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+            >
+            </el-switch>
+          </el-form-item>
+          <template v-if="isSeckill">
+            <el-form-item label="开始时间">
+              <el-date-picker
+                type="date"
+                placeholder="选择日期时间"
+                value-format="yyyy-MM-dd"
+              />
+            </el-form-item>
+            <el-form-item label="结束时间">
+              <el-date-picker
+                type="date"
+                placeholder="选择日期时间"
+                value-format="yyyy-MM-dd"
+              />
+            </el-form-item>
+            <el-form-item label="秒杀价">
+              <el-input type="number">
+                <template slot="append">元</template>
+              </el-input>
+            </el-form-item>
+            <el-form-item label="秒杀库存">
+              <el-input type="number">
+                <template slot="append">个</template>
+              </el-input>
+            </el-form-item>
+          </template>
         </div>
         <div v-show="activeStep === 4">
-          课程详情
+        <el-form-item label="课程详情">
+          <el-input type="textarea"></el-input>
+        </el-form-item>
           <el-form-item>
             <el-button type="primary">保存</el-button>
           </el-form-item>
@@ -78,12 +143,15 @@
 
 <script lang="ts">
 import Vue from 'vue'
+// import {
+//   saveOrUpdateCourse
+// } from '@/services/course'
 
 export default Vue.extend({
   name: 'CourseCreate',
   data () {
     return {
-      activeStep: 1,
+      activeStep: 0,
       steps: [
         { title: '基本信息' },
         { title: '课程封面' },
@@ -91,7 +159,45 @@ export default Vue.extend({
         { title: '秒杀活动' },
         { title: '课程详情' }
       ],
-      imageUrl: '' // 预览图片地址
+      imageUrl: '', // 预览图片地址
+      isSeckill: false, // 是否开启秒杀
+      course: {
+        // id: 0,
+        courseName: '',
+        brief: '',
+        teacherDTO: {
+          // id: 0,
+          // courseId: 0,
+          teacherName: '',
+          teacherHeadPicUrl: '',
+          position: '',
+          description: ''
+        },
+        courseDescriptionMarkDown: '',
+        price: 0,
+        discounts: 0,
+        priceTag: '',
+        discountsTag: '',
+        isNew: true,
+        isNewDes: '',
+        courseListImg: '',
+        courseImgUrl: '',
+        sortNum: 0,
+        previewFirstField: '',
+        previewSecondField: '',
+        status: 0, // 0：未发布，1：已发布
+        sales: 0,
+        activityCourse: false, // 是否开启活动秒杀
+        activityCourseDTO: {
+          // id: 0,
+          // courseId: 0,
+          beginTime: '',
+          endTime: '',
+          amount: 0,
+          stock: 0
+        },
+        autoOnlineTime: ''
+      }
     }
   },
   methods: {
