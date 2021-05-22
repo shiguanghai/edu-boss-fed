145188b7a780eb4a543629ac5a08cbca7e10326a
shiguanghai <1932275668@qq.com>
2021-05-22 17:16:59 +0800

    添加课程完成 富文本版本降低 新增编辑器内部上传文件

--- src/components/TextEditor/index.vue
+++ src/components/TextEditor/index.vue
@@ -5,6 +5,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import E from 'wangeditor'
+import { uploadCourseImage } from '@/services/course'
 
 export default Vue.extend({
   name: 'TextEditor',
@@ -30,6 +31,20 @@ export default Vue.extend({
 
       // 设置初始值 必须在 create 之后
       editor.txt.html(this.value)
+
+      // 上传图片
+      editor.config.customUploadImg =
+      async function (resultFiles: any, insertImgFn: any) {
+        // resultFiles 是 input 中选中的文件列表
+        // insertImgFn 是获取图片 url 后，插入到编辑器的方法
+
+        // 1. 把用户选择的 resultFiles 上传到服务器
+        const fd = new FormData()
+        fd.append('file', resultFiles[0])
+        const { data } = await uploadCourseImage(fd)
+        // 2. 上传图片，返回结果，将图片插入到编辑器中
+        insertImgFn(data.data.name)
+      }
     }
   }
 })
